0afca4d0c391ec5dd714f76f11803c72471211b8
Reed Rosenbluth <Reed.Rosenbluth@gmail.com>
2016-04-19 16:07:47 -0400

    removed counter

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
-import counter from './counter';
 import url from './addressBar';
 
 const rootReducer = combineReducers({
-  counter,
   url,
   routing
 });
